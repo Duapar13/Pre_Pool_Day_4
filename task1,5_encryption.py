3e92c01c9e9f852e037870b96bcb081b44056c2c
Akkranne <brapaud13008@gmail.com>
2023-09-14 14:40:14 +0200

    encryption

---
+++ task1,5_encryption.py
@@ -0,0 +1,22 @@
+def caesar(text, key):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                encrypted_char = chr(((ord(char) - ord('a') - key) % 26) + ord('a'))
+            else:
+                encrypted_char = chr(((ord(char) - ord('A') - key) % 26) + ord('A'))
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+
+mot = input("Ecris la phrase à crypter : ")
+key = int(input("Ecris une clé de décalage (comprise entre 1 et 25) : "))
+
+while key < 1 or key > 25:
+    key = int(input("La clé doit être comprise entre 1 et 25 : "))
+
+encrypted_text = caesar(mot, key)
+print("Phrase cryptée : ", encrypted_text)
